795c3945a6b81024d2420b1247e05fa4bc0738cd
hi <72430078+DoingDog@users.noreply.github.com>
2023-04-24 21:06:00 +0800

    change searching mark

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
-
 const openaikey = 'Bearer sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 const myua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36';
 const apipassword = 'Bearer 12345678';
-
 async function handleRequest(request) {
     const mbody = await request.clone().text();
     const url = new URL(request.url);
@@ -48,27 +46,60 @@ async function handleRequest(request) {
             }
             const lastMessage = messages[messages.length - 1];
             const lastMessageContent = lastMessage.content;
-            if (lastMessageContent.startsWith('WEB1 ')) {
-                const query = lastMessageContent.substring(5, 55);
-                const limit = 5;
-                const searchUrl = `https://ddg-api.herokuapp.com/search?query=${query}&limit=${limit}`;
-                const searchResponse = await fetch(searchUrl);
-                const searchResults = await searchResponse.json();
+            if (lastMessageContent.match(/WS\[.+\]/g)) {
+                const lastSecondUserMessage = messages[messages.length - 3];
+                let lastSecondUserMessageContent = '';
+                if (lastSecondUserMessage) {
+                    lastSecondUserMessageContent = lastSecondUserMessage.content;
+                }
+                let queries = [];
+                let count = 0;
+                const matches = lastMessageContent.match(/WS\[([^\]]+)\]/g);
+                for (let match of matches) {
+                    const query = match.slice(3, -1);
+                    if (count < 2) {
+                        queries.push(query);
+                        count++;
+                    }
+                }
+                count = 0;
+                if (lastSecondUserMessage) {
+                    const matches2 = lastSecondUserMessageContent.match(/WS\[([^\]]+)\]/g);
+                    for (let match of matches2) {
+                        const query = match.slice(3, -1);
+                        if (count < 2) {
+                            queries.push(query);
+                            count++;
+                        }
+                    }
+                }
+                let limit = 2;
+                let snippets = [];
+                if (queries.length <= 2) {
+                    limit = 4;
+                }
+                for (let query of queries) {
+                    const searchUrl = `https://ddg-api.herokuapp.com/search?query=${query}&limit=${limit}`;
+                    const searchResponse = await fetch(searchUrl);
+                    const searchResults = await searchResponse.json();
+                    const currentSnippet = searchResults.map(result => `${result.snippet}`).join('\n');
+                    const currentSnippetWithQuery = `\n\n[${query}]\n${currentSnippet}`;
+                    snippets.push(currentSnippetWithQuery);
+                }
                 const currentDate = new Date();
-                const snippets = searchResults.map(result => `'${result.snippet}'`).join(',');
+                const instructions =
+                    "Instructions: Answer me in the language used in my request or question above. Answer the questions or requests I made above in a comprehensive way. Below are some web search results. Use them if you need.";
+                const regexws = /WS\[[^\]]*\]/g;
+                const replcedLastMessageContent = lastMessageContent.replace(regexws, '');
                 const newLastMessageContent =
-                    `${lastMessageContent.substring(5)}\n\ncurrent date:${currentDate.toLocaleString()} UTC\n\nInstructions: Here are some extra web search results below, use them if needed.\n${snippets}`;
-
+                    `${replcedLastMessageContent}\n\nCurrent date:${currentDate.toLocaleString()} UTC\n\n${instructions}\n${snippets}`;
                 lastMessage.content = newLastMessageContent;
                 requestBody.messages[messages.length - 1] = lastMessage;
-
                 const encoder = new TextEncoder();
                 const uint8Array = encoder.encode(JSON.stringify(requestBody));
-
                 modifiedRequest = new Request(modifiedRequest, {
                     body: JSON.stringify(requestBody)
                 });
-
             }
         }
         const response = await fetch(modifiedRequest);
@@ -76,7 +107,6 @@ async function handleRequest(request) {
         modifiedResponse.headers.set('Access-Control-Allow-Origin', headers_Origin);
         return modifiedResponse;
     }
-
     return new Response('Not Found', {
         status: 404
     });
